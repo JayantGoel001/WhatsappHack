a9552860c751b9a518e6c52882f0ae9966243b30
Jayant Goel <jgoel92@gmail.com>
2021-06-28 22:34:11 +0530

    Initial Commit

--- app/src/main/java/com/example/whatsappopener/MainActivity.kt
+++ app/src/main/java/com/example/whatsappopener/MainActivity.kt
@@ -13,47 +13,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var number:String=""
-        if(intent.action== Intent.ACTION_PROCESS_TEXT)
-        {
+        var number =""
+        if(intent.action== Intent.ACTION_PROCESS_TEXT) {
             number=intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)!!.toString()
         }
-        if(number.contains(" ")) {
-            if(number.length==11) {
-                number = "91"+number.substring(0, 5) + number.substring(6)
-                Log.i("NUMBER", number)
-            }
-            else if(number.length==14)
-            {
-                number=number.substring(0,2)+number.substring(3,8)+number.substring(9)
-                Log.i("NUMBER",number)
-            }
-            else if(number.length==15)
-            {
-                number=number.substring(1,3)+number.substring(4,9)+number.substring(10)
-                Log.i("NUMBER",number)
+        var correctNumber = ""
+        for(i in number.indices){
+            if (number[i] in '0'..'9') {
+                correctNumber+=number[i]
             }
         }
-        if (number.isDigitsOnly()) {
-            startWhatsAPP(number)
+        correctNumber = correctNumber.substring(correctNumber.length-10,correctNumber.length)
+        correctNumber = "91$correctNumber"
+        Log.d("Number", correctNumber)
+
+        if (correctNumber.isDigitsOnly()) {
+            startWhatsAPP(correctNumber)
         } else {
             Toast.makeText(this, "Please Check the Number", Toast.LENGTH_SHORT).show()
         }
-
     }
 
     private fun startWhatsAPP(number: String) {
         val intent=Intent(Intent.ACTION_VIEW)
         intent.setPackage("com.whatsapp")
-        val num =number
-        intent.data= Uri.parse("https://wa.me/$num")
-        if(packageManager.resolveActivity(intent,0)!=null)
-        {
+        intent.data= Uri.parse("https://wa.me/$number")
+        if(packageManager.resolveActivity(intent,0)!=null) {
             startActivity(intent)
-        }
-        else
-        {
-            Toast.makeText(this,"Please Install WhatsAPP",Toast.LENGTH_SHORT).show()
+        }  else {
+            Toast.makeText(this,"Please Install WhatsApp",Toast.LENGTH_SHORT).show()
         }
         finish()
     }
